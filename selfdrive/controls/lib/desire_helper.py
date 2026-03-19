18d82c51d571c3a491d5413caf26631c87f3bb37
royjr <royjr96@gmail.com>
2026-03-19 00:39:19 -0400

    rename

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -55,19 +55,19 @@ class DesireHelper:
 
   @staticmethod
   def get_lane_change_direction(CS):
-    return LaneChangeDirection.left if CS.leftButtonBlinker else LaneChangeDirection.right
+    return LaneChangeDirection.left if CS.rlWindow else LaneChangeDirection.right
 
   def update(self, carstate, lateral_active, lane_change_prob):
     self.alc.update_params()
     self.lane_turn_controller.update_params()
     v_ego = carstate.vEgo
     one_blinker = carstate.leftBlinker != carstate.rightBlinker
-    one_button_blinker = carstate.leftButtonBlinker != carstate.rightButtonBlinker
+    one_button_blinker = carstate.rlWindow != carstate.rrWindow
     below_lane_change_speed = v_ego < LANE_CHANGE_SPEED_MIN
 
     # Lane turn controller update
     self.lane_turn_controller.update_lane_turn(blindspot_left=carstate.leftBlindspot, blindspot_right=carstate.rightBlindspot,
-                                               left_blinker=carstate.leftButtonBlinker, right_blinker=carstate.rightButtonBlinker, v_ego=v_ego)
+                                               left_blinker=carstate.rlWindow, right_blinker=carstate.rrWindow, v_ego=v_ego)
     self.lane_turn_direction = self.lane_turn_controller.get_turn_direction()
 
     if not lateral_active or self.lane_change_timer > LANE_CHANGE_TIME_MAX or self.alc.lane_change_set_timer == AutoLaneChangeMode.OFF:
