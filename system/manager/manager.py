3899cd9aee38a97b00f5a0f3cd711ea4c3ee75ad
J506918 <763986756@qq.com>
2026-04-19 06:09:18 +0800

    Remove font update check from manager_init
    Removed the call to ensure_fonts_up_to_date during manager initialization.

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -23,7 +23,6 @@ from openpilot.system.hardware.hw import Paths
 from openpilot.system.hardware import PC
 
 from openpilot.sunnypilot.system.params_migration import run_migration
-from openpilot.selfdrive.assets.fonts.process import ensure_fonts_up_to_date
 
 
 def manager_init() -> None:
@@ -54,7 +53,7 @@ def manager_init() -> None:
 
   if not PC:
     run_migration(params)
-    ensure_fonts_up_to_date(params)
+
 
   # set unset params to their default value
   for k in params.all_keys():
