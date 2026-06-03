727f27b65e13cdbdce6d0fdf1e2fbfa9ba2c9e52
Wuying Created Local Users <admin@rwqs3hhu6ml3lms.CN-HANGZHOU-198-13-1.WUYING.LOCAL>
2026-06-04 07:49:46 +0800

    fix: whitelist HybridLateralControl param to prevent UI crash on boot

--- common/params_keys.h
+++ common/params_keys.h
@@ -201,6 +201,13 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     // Neural Network Lateral Control
     {"NeuralNetworkLateralControl", {PERSISTENT | BACKUP, BOOL, "0"}},
 
+    // Hybrid Lateral Control
+    {"HybridLateralControl", {PERSISTENT | BACKUP, BOOL, "0"}},
+    {"HybridBlendAggressiveness", {PERSISTENT | BACKUP, INT, "50"}},
+    {"HybridCurveSensitivity", {PERSISTENT | BACKUP, INT, "50"}},
+    {"HybridLookaheadDistance", {PERSISTENT | BACKUP, INT, "8"}},
+    {"HybridStabilityFactor", {PERSISTENT | BACKUP, INT, "100"}},
+
     // sunnylink params
     {"EnableSunnylinkUploader", {PERSISTENT | BACKUP, BOOL}},
     {"LastSunnylinkPingTime", {CLEAR_ON_MANAGER_START, INT}},
